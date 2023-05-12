e4a0891bf64983823d5da31ed8696aed84ae73ce
marco <marcoeribes@gmail.com>
2023-05-12 09:26:12 -0700

    added waitForAsync and removed two tests from app.component.spec.ts

--- src/app/pages/loader/loader.page.spec.ts
+++ src/app/pages/loader/loader.page.spec.ts
@@ -1,17 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { LoaderPage } from './loader.page';
 
 describe('LoaderPage', () => {
   let component: LoaderPage;
   let fixture: ComponentFixture<LoaderPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     fixture = TestBed.createComponent(LoaderPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
+  it('should go to login page after load', () => {
+    expect(true).toBeFalsy();
+  })
 });
