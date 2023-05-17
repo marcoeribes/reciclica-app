14c45425921faff8339de54d4590566d98e1c9df
marco <marcoeribes@gmail.com>
2023-05-17 11:14:54 -0700

    updated state management and auth service

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,50 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginPageForm } from './login.page.form';
 import { Store } from '@ngrx/store';
 import { show, hide } from 'src/store/loading/loading.actions';
 import { AppState } from 'src/store/AppState';
-import { login, recoverPassword } from 'src/store/login/login.actions';
+import { login, recoverPassword, recoverPasswordFail, recoverPasswordSuccess } from 'src/store/login/login.actions';
 import { ToastController } from '@ionic/angular';
 import { LoginState } from 'src/store/login/loginState';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements OnInit, OnDestroy {
 
   form: FormGroup;
+  loginStateSubscription: Subscription;
 
   constructor(private router: Router, private formBuilder: FormBuilder, private store: Store<AppState>, 
-    private toastController: ToastController) { }
+    private toastController: ToastController, private authService: AuthService) { }
 
   ngOnInit() {
     this.form = new LoginPageForm(this.formBuilder).createForm();
 
-    this.store.select('login').subscribe(loginState => {
+    // subscribed for entire duration of component lifespan
+    this.loginStateSubscription = this.store.select('login').subscribe(loginState => {
       this.onIsRecoveringPassword(loginState);
-      this.onRecoveredPassword(loginState);
+      this.onRecoveredPasswordFailed(loginState);
+      this.onRecoveredPasswordSuccess(loginState);
     })
+  }
+
+  ngOnDestroy() {
+    if (this.loginStateSubscription){
+      this.loginStateSubscription.unsubscribe();
+    }
   }
 
   private onIsRecoveringPassword(loginState: LoginState){
     if (loginState.isRecoveringPassword){
       this.store.dispatch(show());
+      this.authService.recoverEmailPassword(this.form.get('email')?.value).subscribe(() => {
+        this.store.dispatch(recoverPasswordSuccess());
+      }, error => {
+        this.store.dispatch(recoverPasswordFail({error}))
+      });
     }
   }
 
-  private async onRecoveredPassword(loginState: LoginState){
-    if (loginState.isRecoveringPassword){
-      this.store.dispatch(hide());
+  private async onRecoveredPasswordSuccess(loginState: LoginState){
+    if (loginState.recoveredPassword){
       const toaster = await this.toastController.create({
         position: "bottom",
         message: "Recovery email sent",
         color: "primary"
       });
       toaster.present();
+      this.store.dispatch(hide());
+    }
+  }
+
+  private async onRecoveredPasswordFailed(loginState: LoginState){
+    if (loginState.error){
+      const toaster = await this.toastController.create({
+        position: "bottom",
+        message: loginState.error.message,
+        color: "danger"
+      });
+      toaster.present();
+      this.store.dispatch(hide());
     }
   }
 
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,16 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  recoverEmailPassword(email: string) : Observable<void> {
+    return new Observable<void>(observer => {
+      setTimeout(() => {
+        if (email == "error@email.com"){
+          observer.error({message: "Email not found"});
+        }
+        observer.next();
+        observer.complete();
+      }, 3000);
+    })
+  }
 }
