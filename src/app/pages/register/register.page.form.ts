f7165f3877d98cf3d23ffe1ad85d7fe2cce4c532
marco <marcoeribes@gmail.com>
2023-05-22 22:17:05 -0700

    fixed confirmation password valdiaion

--- src/app/pages/register/register.page.form.ts
+++ src/app/pages/register/register.page.form.ts
@@ -13,15 +13,20 @@ export class RegisterPageForm {
 
     createForm() : FormGroup {
         let form = this.formBuilder.group({
-            name: ['', [Validators.required]],
+            name: ['', [Validators.required, Validators.pattern('^[a-zA-Z]*$')]],
             email: ['', [Validators.required, Validators.email]],
             password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]],
-            confirmPassword: ['', [Validators.required]]
-        });
+            confirmPassword: ['', Validators.required],
 
-        //form.get('name')?.setValidators(firstAndLastName(form));
-        //form.get('confirmPassword')?.setValidators(matchPasswords(form));
+        },
+
+        );
+
+        //form.get('name')?.addValidators(firstAndLastName(form), );
+        //form.controls['name']?.addValidators([firstAndLastName(form)])
+        form.controls['confirmPassword']?.addValidators([matchPasswords(form)]);
         //form.get('name')?.setValidators(noSpecialCharactersAllowed(form));
+        console.log('here');
 
         return form;
     }
@@ -32,28 +37,43 @@ export class RegisterPageForm {
 
 }
 
-function firstAndLastName(form: FormGroup) : ValidatorFn {
-    const name = form.get('name');
-    const validatorFn = () => {
-        return (/^[A-Za-z]+\s[A-Za-z]+$/).test(name?.value) ? null : {noLastName: true}
-    }
-
-    return validatorFn;
-}
-
-/*function noSpecialCharactersAllowed(form: FormGroup) : ValidatorFn {
-    const name = form.get('name');
-    const validatorFn = () => {
-        return name?.value.match('/^[A-Za-z\s]+$/') ? null : {noSpecialCharcters: true}
-    }
-    return validatorFn;
-}*/
-
 function matchPasswords(form: FormGroup) : ValidatorFn {
     const password = form.get('password');
     const confirmPassword = form.get('confirmPassword');
+    console.log("confpass", confirmPassword)
     const validatorFn = () => {
-        return password?.value === confirmPassword?.value  ? null : {doesNotMatch: true}
+        return confirmPassword?.value === password?.value  ? null : {doesNotMatch: true}
     }
     return validatorFn;
 }
+
+function firstAndLastName(form: FormGroup) : ValidatorFn {
+    const name = form.get('name');
+    const validatorFn = () => {
+        return (/.(?= .)/).test(name?.value) ? null : {noLastName: true}
+    }
+    return validatorFn;
+}
+
+function noSpecialCharactersAllowed(form: FormGroup) : ValidatorFn {
+    const name = form.get('name');
+    const validatorFn = () => {
+        return name?.value.match('^[a-zA-Z\s]*$') ? null : {noSpecialCharcters: true}
+    }
+    return validatorFn;
+}
+
+
+
+/*function matchPasswords() : ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+        const password = control.get('password');
+        const confirmPassword = control.get('confirmPassword');
+
+        if (password && confirmPassword && password.value !== confirmPassword.value){
+            return {doesNotMatch: true};
+        }
+
+        return null;
+    }
+}*/
