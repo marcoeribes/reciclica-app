053f6ca66bf4277e8bd7422e70e62063c7b91695
marco <marcoeribes@gmail.com>
2023-05-24 01:03:56 -0700

    Added even more functionality to modal

--- src/app/components/create-post/create-post.component.ts
+++ src/app/components/create-post/create-post.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActionSheetController } from '@ionic/angular';
+import { Component, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { ActionSheetController, IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-create-post',
@@ -7,30 +7,51 @@ import { ActionSheetController } from '@ionic/angular';
   styleUrls: ['./create-post.component.scss'],
 })
 export class CreatePostComponent  implements OnInit {
+  @ViewChild(IonModal) modal: IonModal;
   @Input() openModal: string;
+  @Output() content: string = '';
   presentingElement = undefined;
+
 
   constructor(private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {}
 
-  canDismiss = async () => {
-    const actionSheet = await this.actionSheetCtrl.create({
-      header: 'Discard Post? Post won\'t be save',
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-        },
-        {
-          text: 'Discard',
-          role: 'discard',
-        },
-      ],
-    });
-
-    actionSheet.present();
-    const { role } = await actionSheet.onWillDismiss();
-    return role === 'discard';
+  isPostEmpty(content: string){
+    return !content ? true : false;
   }
+
+  createPost(){
+    this.canDismiss = async () => true;
+    this.modal.dismiss();
+  }
+
+  exitModal(){
+    this.canDismiss = async () => {
+      const actionSheet = await this.actionSheetCtrl.create({
+        header: 'Discard Post? Post won\'t be save',
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+          },
+          {
+            text: 'Discard',
+            role: 'discard',
+          },
+        ],
+      });
+  
+      actionSheet.present();
+      const { role } = await actionSheet.onWillDismiss();
+      return role === 'discard';
+    }
+    this.modal.dismiss();
+  }
+
+  customCounterFormatter(inputLength: number, maxLength: number) {
+    return `${maxLength - inputLength} characters remaining`;
+  }
+
+  canDismiss = async () => false;
 }
--- src/app/components/create-post/create-post.module.ts
+++ src/app/components/create-post/create-post.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 import { CreatePostComponent } from './create-post.component';
 
@@ -10,7 +11,8 @@ import { CreatePostComponent } from './create-post.component';
   declarations: [CreatePostComponent],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule, 
+    FormsModule
   ],
   exports: [
     CreatePostComponent
