6bb614d792a945050ee6cfdcd13d3cc258d243d7
marco <marcoeribes@gmail.com>
2023-05-31 11:01:17 -0700

    working sidebar

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-side-bar',
@@ -6,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-bar.component.scss'],
 })
 export class SideBarComponent  implements OnInit {
+  handlerMessage = '';
+  roleMessage = '';
 
-  constructor() { }
+  constructor(private router: Router, private menuCtrl: MenuController) { }
 
   ngOnInit() {}
 
+
+  logout() {
+    this.menuCtrl.enable(false);
+    this.menuCtrl.enable(true);
+  }
+
+  public alertButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      handler: () => {},
+    },
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: () => {
+        this.router.navigate(['login']);
+      },
+    },
+  ];
 }
